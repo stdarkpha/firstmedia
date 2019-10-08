4f8a9a4c5bb960413c2b83bf8230e66d3168349c
stdarkpha <farouqsimabua@gmail.com>
2019-10-08 22:26:01 +0700

    New Files

--- js/js.js
+++ js/js.js
@@ -1,5 +1,4 @@
 var swiper = new Swiper('.swiper-container', {
-  loop: true,
   autoplay: {
   	delay: 6000,
     disableOnInteraction: false,
@@ -10,21 +9,56 @@ var swiper = new Swiper('.swiper-container', {
   },
 });
 
-//Script Change Color
 let html = document.querySelector(`html`);
 
 var color = ['#3B60AE', '#E11F28', '#F8981D', '#3B60AE', '#1DAD4B'];
 var dark = ['#1d4089', '#9e0056', '#c7002a','#1d4089', '#357e00'];
+var hue = ['0' , '130', '165', '0', '245'];
 var i =0;
 
-let uid = setInterval(() => {
-    i = (i+1)%color.length;
-    i = (i+1)%dark.length;
-    change = color[i];
-    change2 = dark[i];
+setInterval(() => {
+if($('#slide1').hasClass('swiper-slide-active')){
+  change = color[0];
+  change2 = dark[0];
+  change3 = hue[0];
+
     html.style.setProperty(`--theme`, `${change}`);
     html.style.setProperty(`--dark`, `${change2}`);
-}, 6000);
+    html.style.setProperty(`--shift`, `${change3}deg`);
+}else if($('#slide2').hasClass('swiper-slide-active')){
+  change = color[1];
+  change2 = dark[1];
+  change3 = hue[1];
+
+    html.style.setProperty(`--theme`, `${change}`);
+    html.style.setProperty(`--dark`, `${change2}`);
+    html.style.setProperty(`--shift`, `${change3}deg`);
+}else if($('#slide3').hasClass('swiper-slide-active')){
+  change = color[2];
+  change2 = dark[2];
+  change3 = hue[2];
+
+    html.style.setProperty(`--theme`, `${change}`);
+    html.style.setProperty(`--dark`, `${change2}`);
+    html.style.setProperty(`--shift`, `${change3}deg`);
+}else if($('#slide4').hasClass('swiper-slide-active')){
+  change = color[3];
+  change2 = dark[3];
+  change3 = hue[3];
+
+    html.style.setProperty(`--theme`, `${change}`);
+    html.style.setProperty(`--dark`, `${change2}`);
+    html.style.setProperty(`--shift`, `${change3}deg`);
+}else{
+  change = color[4];
+  change2 = dark[4];
+  change3 = hue[4];
+
+    html.style.setProperty(`--theme`, `${change}`);
+    html.style.setProperty(`--dark`, `${change2}`);
+    html.style.setProperty(`--shift`, `${change3}deg`);
+}
+  }, 500);
 
 $(window).on("scroll", function() {
     if($(window).scrollTop()) {
